c97a931aca8bb9eded7bc718d44fe9e955990907
kamkow1 <ikamileq@gmail.com>
2022-03-27 07:41:00 -0700

    added route guard in upload component

--- ClientApp/src/components/Upload/Upload.tsx
+++ ClientApp/src/components/Upload/Upload.tsx
@@ -2,9 +2,12 @@
 import UserStore from "../../stores/UserStore";
 import UploadForm from './UploadForm';
 import './Upload.scss';
+import { Navigate } from "react-router-dom";
 
 export default class Upload extends React.Component<{ store: UserStore }> {
     render() {
+        if (Object.keys(this.props.store.user).length === 0) return <Navigate to="/login" />
+
         return (
             <div className="upload-form-wrapper">
                 <h3>upload your track</h3>
